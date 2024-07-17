9b5a529daabc26d14c3a8f18381b79e6e41dee2e
Alexander <minkeviiich@gmail.com>
2024-07-18 02:05:55 +0300

    TestWeather

---
+++ weather/views.py
@@ -0,0 +1,29 @@
+import requests 
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    key = 'fb787564dad8696a2c56dbed3fe52a28'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=' + key
+
+    if (request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+    all_cities = []
+    
+    for city in cities:
+        resp = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': resp['main']['temp'],
+            'icon': resp['weather'][0]['icon'],
+
+        }
+        all_cities.append(city_info)
+    context = {'all_info': all_cities, 'form': form}
+    return render(request, 'weather/index.html', context)
